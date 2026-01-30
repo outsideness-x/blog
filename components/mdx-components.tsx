62c755a3222c3bf8685631cd384ab5d8b2923b75
outsideness-x <kravchenkoalex02@gmail.com>
2026-01-30 16:34:45 +0300

    some fix

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -2,12 +2,22 @@ import Link from "next/link";
 import Image from "next/image";
 import { ManimVideo } from "./manim-video";
 
-// custom components available in mdx
 export const components = {
   Image,
   Link,
   ManimVideo,
-  // override default html elements for styling
+  // 
+  img: (props: any) => (
+    <Image
+      sizes="100vw"
+      style={{ width: '100%', height: 'auto' }}
+      width={800}
+      height={400}
+      className="rounded-lg border border-border my-4"
+      {...props}
+    />
+  ),
+  //
   h1: (props: any) => <h1 className="text-2xl font-bold mt-8 mb-4 text-primary" {...props} />,
   h2: (props: any) => <h2 className="text-xl font-bold mt-6 mb-3 text-zinc-100" {...props} />,
   p: (props: any) => <p className="leading-7 mb-4 text-zinc-300" {...props} />,
